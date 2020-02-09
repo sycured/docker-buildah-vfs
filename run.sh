bef89012a58b1d5044be9d989ba9559a1f73086b
sycured <60801403+sycured@users.noreply.github.com>
2020-02-09 14:34:15 -0500

    init

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+mkimg=$(buildah from fedora:rawhide)
+buildah config --author='sycured' $mkimg
+buildah config --label Name='buildah-vfs' $mkimg
+buildah run "$mkimg" -- dnf upgrade -y
+buildah run "$mkimg" -- dnf install buildah git podman skopeo -y
+mntimg=$(buildah mount $mkimg)
+rm -rf $mntimg/var/cache/dnf/*
+cp storage.conf $mntimg/etc/containers/storage.conf
+buildah unmount $mkimg
+buildah commit --squash "$mkimg" "buildah-vfs"
+buildah rm "$mkimg"
