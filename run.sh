f4046f6305cbb5f800c6a3d3144563d884ff6b0f
sycured <60801403+sycured@users.noreply.github.com>
2020-02-17 22:07:02 -0500

    add --nogpg because we use rawhide

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ set -x
 mkimg=$(buildah from fedora:rawhide)
 buildah config --author='sycured' $mkimg
 buildah config --label Name='buildah-vfs' $mkimg
-buildah run "$mkimg" -- dnf upgrade -y
-buildah run "$mkimg" -- dnf install buildah git podman skopeo -y
+buildah run "$mkimg" -- dnf upgrade -y --nogpg
+buildah run "$mkimg" -- dnf install buildah git podman skopeo -y --nogpg
 mntimg=$(buildah mount $mkimg)
 rm -rf $mntimg/var/cache/dnf/*
 cp storage.conf $mntimg/etc/containers/storage.conf
