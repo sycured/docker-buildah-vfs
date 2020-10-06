5e7ed4275d542a684d174acc208f024695708c99
sycured <60801403+sycured@users.noreply.github.com>
2020-10-06 15:17:19 -0500

    fix quotes

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -x
 mkimg=$(buildah from fedora:rawhide)
-buildah config --author='sycured' $mkimg
-buildah config --label Name='buildah-vfs' $mkimg
+buildah config --author='sycured' "$mkimg"
+buildah config --label Name='buildah-vfs' "$mkimg"
 buildah run "$mkimg" -- dnf upgrade -y --nogpg
 buildah run "$mkimg" -- dnf install buildah git podman skopeo -y --nogpg
 buildah run "$mkimg" -- mkdir /shared
-buildah config --workingdir='/shared' $mkimg
-mntimg=$(buildah mount $mkimg)
-rm -rf $mntimg/var/cache/dnf/*
-cp storage.conf $mntimg/etc/containers/storage.conf
-buildah unmount $mkimg
+buildah config --workingdir='/shared' "$mkimg"
+mntimg=$(buildah mount "$mkimg")
+rm -rf "$mntimg"/var/cache/dnf/*
+cp storage.conf "$mntimg"/etc/containers/storage.conf
+buildah unmount "$mkimg"
 buildah commit --squash "$mkimg" "buildah-vfs"
 buildah rm "$mkimg"
